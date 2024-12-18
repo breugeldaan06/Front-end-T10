590a9a4c9e5d9928881f1224668e74828cd35556
Daan Breugel <daan.vanbreugel1@edu-kw1c.nl>
2024-12-18 09:45:12 +0100

    bootstrap toegevoegd

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 createApp(App).mount('#app')
