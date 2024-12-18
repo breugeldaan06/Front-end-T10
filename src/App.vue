3d86b9b87c802e0e897aa68665b75c140e15fb4e
Daan Breugel <daan.vanbreugel1@edu-kw1c.nl>
2024-12-18 09:29:57 +0100

    eerste commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>Hallo wereld</h1>
+  <div>
+    <p>
+      Meneer Saebu moet mij even helpen!!<br>
+      <button @click="groet">Klik me</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    groet() {
+      alert('!');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: black;
+}
+</style>
